f6c509e1f84909cf6c04f177236c65f03ba6a0aa
RikoshetGames <ilyalew@mail.ru>
2023-12-17 23:04:32 +0300

    MyDjango

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from catalog.views import index
+from catalog.views import index, contact, home
 
 urlpatterns = [
-    path('', index)
+    # path('', index),
+    # path('', contact),
+    path('', home)
 ]
