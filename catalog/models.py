7444a605f2227911ecb53521e5652b8742dd3b9c
RikoshetGames <ilyalew@mail.ru>
2023-12-21 22:29:02 +0300

    MyDjango

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+NULLABLE = {'null': True, 'blank': True}
+
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=50, verbose_name='Наименование продукта')
+    product_description = models.TextField(max_length=100, verbose_name='Описание продукта')
+    product_image = models.ImageField(upload_to='product_images', verbose_name='Изображение продукта', **NULLABLE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    product_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена продукта за единицу')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания записи')  # Automatically set the date of
+    date_last_modified = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения записи')  # Automatically set\
+
+    def __str__(self):
+        return f'{self.product_name} - {self.product_description}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, verbose_name='Наименование категории')
+    category_description = models.TextField(max_length=100, verbose_name='Описание категории')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания записи')
+
+    def __str__(self):
+        return f'{self.category_name} - {self.category_description}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w41_blh%e4&ws=s#rm1n_#(*8$p=7+so%z8xfvlul$c6vcj2^h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'firstdjango',
+        'USER': 'postgres',
+        'PASSWORD': '645595',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -123,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
