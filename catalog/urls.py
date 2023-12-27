5b70c93ad018850eee11fa54208adfc12c1c45f9
RikoshetGames <ilyalew@mail.ru>
2023-12-27 22:41:01 +0300

    Update catalog update view.py:refactor def contact in class ContactView update urls.py: new link ContactView

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from catalog.views import index, contact, home, ProductListView, CategoryListView, ProductDetailView
+from catalog.views import index, home, ProductListView, CategoryListView, ProductDetailView, ProductCategoryListView, ContactView
 
 urlpatterns = [
     path('product_list/', ProductListView.as_view(), name='product_list'),
     path('category_list/', CategoryListView.as_view(), name='category_list'),
     path('index/', index, name='index'),
-    path('contacts/', contact, name='contact'),
+    path('contacts/', ContactView.as_view(), name='contacts'),
     path('', home, name='home'),
+    path('product_list/<int:pk>/', ProductCategoryListView.as_view(), name='product_list'),
     path('product/<int:pk>', ProductDetailView.as_view(), name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
 
@@ -17,6 +17,15 @@ class ProductListView(ListView):
     model = Product
 
 
+class ProductCategoryListView(ListView):
+    model = Product
+
+
+    def get_queryset(self):
+        category_pk = self.kwargs['pk']
+        return Product.objects.filter(category_id=category_pk)
+
+
 class ProductDetailView(DetailView):
     def get(self, request, pk):
         product = Product.objects.get(pk=pk)
@@ -26,13 +35,17 @@ class ProductDetailView(DetailView):
         return render(request, 'catalog/product.html', context)
 
 
-def contact(request):
-    if request.method == 'POST':
+class ContactView(View):
+    def get(self, request):
+        return render(request, 'catalog/contacts.html')
+
+
+    def post(self, request):
         name = request.POST.get('name')
         email = request.POST.get('email')
         if name is not None and email is not None:
             return render(request, 'catalog/contacts.html', context={'name': name, 'email': email})
-    return render(request, 'catalog/contacts.html')
+        return redirect('contacts')
 
 
 def home(request):
