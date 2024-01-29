e0e3d5f338d46a4217db00bd5870907f72f588f7
RikoshetGames <ilyalew@mail.ru>
2024-01-29 23:22:12 +0300

    Cache Update

--- config/settings.py
+++ config/settings.py
@@ -143,7 +143,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv().get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True
 
 CACHES_ENABLED = os.getenv('CACHES_ENABLED') == '1'
